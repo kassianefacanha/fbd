0fbfa703374d7cee920278b2a605fd4aaca60bf2
@kassianefacanha <kassi-kaka@hotmail.com>
2019-11-27 14:13:57 -0300

    up

---
+++ crud/atvbolsistas/functions.php
@@ -0,0 +1,45 @@
+<?php		
+require_once('../conf.php');	
+require_once(DBAPI);		
+$atvbolsistas = null;	
+$atvbolsista = null;
+
+/**	 *  Listagem de atvbolsistas	 */	
+function index() {	
+    global $atvbolsistas;		
+    $atvbolsistas = find_all('atvbolsistas');	
+}
+
+/**	 *  Cadastro de atvbolsistas	 */	
+function add() {		  
+    if (!empty($_POST['atvbolsista'])) {	    	    		    
+        $atvbolsista = $_POST['atvbolsista'];	    	    	    
+        save('atvbolsistas', $atvbolsista);	    
+        header('location: index.php');	 
+     }	
+    }
+    /**	 *	Atualizacao/Edicao de atvbolsista	 */	
+    function edit() {		  	  
+        if (isset($_GET['id'])) {		    
+            $id = $_GET['id'];		    
+            if (isset($_POST['atvbolsista'])) {		      
+                $atvbolsista = $_POST['atvbolsista'];	      	      
+                update('atvbolsistas', $id, $atvbolsista);	      
+                header('location: index.php');	    } 
+                else {		      
+                    global $atvbolsista;	      
+                    $atvbolsista = find('atvbolsistas', 
+                    $id);	    } 	  } 
+                    else {	    
+                        header('location: index.php');	  }	}
+
+    /**	 *  Visualização de um atvbolsista	 */	
+    function view($id = null) {	  
+        global $atvbolsista;	  
+        $atvbolsista = find('atvbolsistas', $id);	
+    }
+    /**	 *  Exclusão de um atvbolsista	 */	
+    function delete($id = null) {		  
+        global $atvbolsista;	 
+         $atvbolsista = remove('atvbolsistas', $id);		 
+          header('location: index.php');	}
---
+++ crud/atvbolsistas/view.php
@@ -0,0 +1,14 @@
+<?php 		require_once('functions.php'); 		view($_GET['id']);	?>	
+<?php include(HEADER_TEMPLATE); ?>	
+<h2>atvbolsista <?php echo $atvbolsista['id']; ?></h2>	<hr>	
+<?php if (!empty($_SESSION['message'])) : ?>		<div class="alert alert-<?php echo $_SESSION['type']; ?>"><?php echo $_SESSION['message']; ?></div>	<?php endif; ?>	
+<dl class="dl-horizontal">		<dt>Nome :</dt>		<dd><?php echo $atvbolsista['name']; ?></dd>	
+	<dt>Horas:</dt>		<dd><?php echo $atvbolsista['horas']; ?></dd>	
+
+
+<div id="actions" class="row">		
+<div class="col-md-12">		 
+<a href="edit.php?id=<?php echo $atvbolsista['id']; ?>" class="btn btn-primary">Editar</a>		  
+<a href="index.php" class="btn btn-default">Voltar</a>		
+</div>	</div>	
+<?php include(FOOTER_TEMPLATE); ?>
